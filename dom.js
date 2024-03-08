510b156fbb35e8df5d312ff5ad9f3d30a20da881
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-03-08 16:39:09 -0600

    Funcionando el estado de mostrar el tiempo

--- dom.js
+++ dom.js
@@ -1,14 +1,17 @@
-import { fechasDiferencias } from "./logica";
+import { newFecha } from "./fechas.js";
 
-console.log(fechasDiferencias);
+
+var time = newFecha;
+console.log(time);
+
 
 const años = document.createElement("h1");
 const meses = document.createElement("h1");
 const dias = document.createElement("h1");
 
-años.innerText = "1 año"
-meses.innerText = `${diferencia.month} ${diferencia.month>1?"mes":"meses"}`
-dias.innerText = "1 "
+años.innerText = `${time.año} ${time.years>1?"mes":"meses"}`
+meses.innerText = `${time.meses} ${time.month>1?"mes":"meses"}`
+dias.innerText = `${time.dias} ${time.days>1?"mes":"meses"}`
 
 document.body.append(años,meses,dias);
 
---
+++ fechas.js
@@ -0,0 +1,24 @@
+import { intervalToDuration } from "./node_modules/date-fns/index.mjs"
+
+const fechas = {
+    start: new Date(2022, 8, 20, 0, 0, 0),
+    end: Date.now()
+}
+
+
+
+function obtenerFechas(){
+    let diferencia = intervalToDuration(fechas)
+    return diferencia
+}
+
+let fechasDiferencias = obtenerFechas();
+
+let newFecha = {
+    año: fechasDiferencias.years,
+    meses: fechasDiferencias.months,
+    dias: fechasDiferencias.days
+}
+
+console.log(newFecha.año);
+export {newFecha}
