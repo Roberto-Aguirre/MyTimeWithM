9fe85744d864ece846f356377dee32f929538dfe
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-03-19 16:49:01 -0600

    Estilos Css

--- dom.js
+++ dom.js
@@ -8,9 +8,9 @@ var time = newFecha;
 // console.log(time);
 
 //Append a divs
-$(".años").append(`<h1>${time.año} ${time.years>1?"mes":"meses"}</h1>`);
+$(".años").append(`<h1>${time.año} ${time.years>1?"año":"años"}</h1>`);
 $(".meses").append(`<h1>${time.meses} ${time.month>1?"mes":"meses"}</h1>`);
-$(".dias").append(`<h1>${time.dias} ${time.days>1?"mes":"meses"}</h1>`);
+$(".dias").append(`<h1>${time.dias} ${time.days>1?"día":"días"}</h1>`);
 
 });
 
