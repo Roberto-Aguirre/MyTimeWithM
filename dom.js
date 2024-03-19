b1e353b44ab26f9d6ea22acbe96b19ea8aeeb828
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-03-19 15:40:23 -0600

    Jquery agregado y formato de html

--- dom.js
+++ dom.js
@@ -1,8 +1,20 @@
 import { newFecha } from "./fechas.js";
+import "./node_modules/jquery/dist/jquery.slim.js"
 
 
+$(document).ready(function () {
+        
 var time = newFecha;
 // console.log(time);
+
+//Append a divs
+$(".años").append(`<h1>${time.año} ${time.years>1?"mes":"meses"}</h1>`);
+$(".meses").append(`<h1>${time.meses} ${time.month>1?"mes":"meses"}</h1>`);
+$(".dias").append(`<h1>${time.dias} ${time.days>1?"mes":"meses"}</h1>`);
+
+});
+
+/* JAVASCRIPT PURO 
 
 const años = document.createElement("h1");
 const meses = document.createElement("h1");
@@ -11,15 +23,7 @@ const dias = document.createElement("h1");
 const divAños = document.querySelector(".años");
 const divMeses = document.querySelector(".meses");
 const divDias = document.querySelector(".dias");
+*/
 
-console.log(divAños,divMeses,divDias);
-
-años.innerText = `${time.año} ${time.years>1?"mes":"meses"}`
-meses.innerText = `${time.meses} ${time.month>1?"mes":"meses"}`
-dias.innerText = `${time.dias} ${time.days>1?"mes":"meses"}`
-
-divAños.appendChild(años);
-divMeses.appendChild(meses);
-divDias.appendChild(dias);
 
 // console.log(años,meses,dias);
