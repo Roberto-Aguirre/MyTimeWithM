d7f56f35f23b31403b7d11d1e8f4c8799e037ca5
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-04-04 12:22:41 -0600

    Animacion basica y flor más centrada, mejora usando destructuracion de variables

--- dom.js
+++ dom.js
@@ -8,12 +8,16 @@ var time = newFecha;
 // console.log(time);
 
 //Append a divs
-$(".años").append(`<h1>${time.año} ${time.years>1?"año":"años"}</h1>`);
-$(".meses").append(`<h1>${time.meses} ${time.month>1?"mes":"meses"}</h1>`);
-$(".dias").append(`<h1>${time.dias} ${time.days>1?"día":"días"}</h1>`);
+const {año,meses,dias} = time
+
+$(".años").append(`<h1>${año} ${año>1?"años":"año"}</h1>`);
+$(".meses").append(`<h1>${meses} ${meses>1?"meses":"mes"}</h1>`);
+$(".dias").append(`<h1>${dias} ${dias>1?"días":"dia"}</h1>`);
 
 });
 
+async ()
+setTimeout(()=>{console.log("h");},1000)
 /* JAVASCRIPT PURO 
 
 const años = document.createElement("h1");
