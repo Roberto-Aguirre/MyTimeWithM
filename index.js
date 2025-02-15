5898a7989288cbab6be92d2f40ca8a0c66e4e111
ext_riaguirres <ext_riaguirres@falabella.com.mx>
2025-02-14 19:05:25 -0600

    Proved librabies

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fechas = require('./utils/fechas')
 
+const {days,months,years} = fechas.obtenerFechas()
 // Serve static files (CSS, JS, etc.) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html when accessing the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-// Start the server on port 3000
+app.get('/', (req, res) => {
+  res.send(`${days}+${months}+${years}`)
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- utils/fechas.js
+++ utils/fechas.js
@@ -1,10 +1,13 @@
-const DatesCalculator = require('date-fns');
+const datesBeetween = require('date-fns');
 
 const fechas = {
     start: new Date(2022, 8, 20, 0, 0, 0),
     end: Date.now()
 }
 
-export default function obtenerFechas(){
-    return DatesCalculator.intervalToDuration(fechas)
+function obtenerFechas(){
+    return datesBeetween.intervalToDuration(fechas)
 }
+
+
+module.exports = {obtenerFechas}
