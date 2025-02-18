1050bc656a1d9eaf4981e742dec483f7c784dbad
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2025-02-18 00:21:30 -0600

    exposed src/static

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { obtenerFechas } = require('./utils/fechas')
+const { obtenerFechas } = require('./src/utils/fechas')
 
 const { date:{days, months, years}, remain } = obtenerFechas()
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'static')));
+const staticPath = '/src/static/styles.css';
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/src/static'));
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.get('/', (req, res) => {
   res.render("index", { days, months, years, remain });
